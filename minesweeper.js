b525e3097acb9b97fa40757fc7bbcb43c2ff8240
Rattlesnake1972 <grattlesnake@gmail.com>
2023-09-02 00:21:12 +0200

    for ciklussal egy sor feltöltése a hidden elemmel

--- minesweeper.js
+++ minesweeper.js
@@ -3,18 +3,11 @@ const canvas = document.getElementById("myCanvas");
 const c = canvas.getContext("2d");
 
 const size = 50;
+const columns = canvas.width / size;
 
-drawImage(0 * size, 0);
-drawImage(1 * size, 0);
-drawImage(2 * size, 0);
-drawImage(3 * size, 0);
-drawImage(4 * size, 0);
-drawImage(5 * size, 0);
-drawImage(6 * size, 0);
-drawImage(7 * size, 0);
-drawImage(8 * size, 0);
-drawImage(9 * size, 0);
-
+for (let i = 0; i < columns; i++) {
+    drawImage(i * size, 0);
+}
 
 
 function drawImage(x, y) {
