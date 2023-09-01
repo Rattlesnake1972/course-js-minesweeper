b730039a82bf42f5b8657f0bb727bf520b80ebd3
Rattlesnake1972 <grattlesnake@gmail.com>
2023-09-02 00:44:49 +0200

    az egész canvas feltöltése a hidden elemmel beáfor

--- minesweeper.js
+++ minesweeper.js
@@ -5,10 +5,11 @@ const c = canvas.getContext("2d");
 const size = 50;
 const columns = canvas.width / size;
 
-for (let i = 0; i < columns; i++) {
-    drawImage(i * size, 0);
+for (let valami = 0; valami < columns; valami++) {
+    for (let i = 0; i < columns; i++) {
+        drawImage(i * size, valami * size);
+    }
 }
-
 
 function drawImage(x, y) {
     c.drawImage(image, x, y, size, size);          
