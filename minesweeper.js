1809ccd1a1c5a8c70851f2bbc272fa034ab87c69
Rattlesnake1972 <grattlesnake@gmail.com>
2023-09-01 23:15:02 +0200

    const, var, és let változók

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,10 @@
-var image = document.getElementById("hidden");
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.drawImage(image, 0, 0);          // A kép bal felső sarka a (0,0) koordinátára kerül
+const image = document.getElementById("hidden");
+const canvas = document.getElementById("myCanvas");
+const c = canvas.getContext("2d");
+
+const size = 50;
+let x = 100;
+let y = 50;
+
+c.drawImage(image, x, y, size, size);          // A kép bal felső sarka a (0,0) koordinátára kerül. Az első szám a zárójelben a vízszintes, a második a függőleges pozíciót jelöli. Az első 50-es szám a vízszintes, a második a függőleges méretet jelöli.
+c.drawImage(image, x+size, y+size, size, size);
