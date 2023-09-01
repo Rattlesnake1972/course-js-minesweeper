bc532cc91faa9831a197471e32576ca2bcfef224
Rattlesnake1972 <grattlesnake@gmail.com>
2023-09-01 15:27:57 +0200

    Canvas vászonra kép elhelyezése

--- minesweeper.js
+++ minesweeper.js
@@ -1,1 +1,4 @@
-console.log("minesweeper.js loaded");
+var image = document.getElementById("hidden");
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.drawImage(image, 0, 0);          // A kép bal felső sarka a (0,0) koordinátára kerül
