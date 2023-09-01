1eee90c625d961e511131c3b70ac46381932080e
Rattlesnake1972 <grattlesnake@gmail.com>
2023-09-01 23:54:02 +0200

    10 db hidden elem (négyzet) kirajzolása egymás mel

--- minesweeper.js
+++ minesweeper.js
@@ -4,9 +4,18 @@ const c = canvas.getContext("2d");
 
 const size = 50;
 
-drawImage(0, 0);
-drawImage(100, 100);
-drawImage(123, 234);
+drawImage(0 * size, 0);
+drawImage(1 * size, 0);
+drawImage(2 * size, 0);
+drawImage(3 * size, 0);
+drawImage(4 * size, 0);
+drawImage(5 * size, 0);
+drawImage(6 * size, 0);
+drawImage(7 * size, 0);
+drawImage(8 * size, 0);
+drawImage(9 * size, 0);
+
+
 
 function drawImage(x, y) {
     c.drawImage(image, x, y, size, size);          
